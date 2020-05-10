9e169212aee0caeff83b1acfbffcbc685c1eb2ec
ankitdarekar <ankitdarekar10@gmail.com>
2020-05-10 14:13:02 +0530

    square root problem

---
+++ square-root-problem/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        #  Using Binary Search , Time Complexity O(logN)
+        if num == 1:
+            return True
+        left, right = 1, num
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid * mid == num:    
+                return True
+            elif mid * mid < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
