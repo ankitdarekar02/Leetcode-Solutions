caea6bdb07e0f56aa6ed784ec1fc40e5b383b3e7
ankitdarekar <ankitdarekar10@gmail.com>
2020-06-13 23:33:15 +0530

    sort colors js solution

--- sort-colors/solution.js
+++ sort-colors/solution.js
@@ -1,16 +1,25 @@
-let left = 0 , right = nums.length -1 , current = 0;
-while(current <= right){
-    if(nums[current] === 0){
-        let temp = nums[left];
-        nums[left++] = nums[current];
-        nums[current++] = temp;
-    }else if(nums[current] === 1){
-        current++;
-    }else{
-        let temp = nums[right];
-        nums[right--] = nums[current];
-        nums[current] = temp;
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let low = 0;
+    let high = nums.length -1;
+    let mid = 0;
+    while(mid <= high){
+        if(nums[mid] == 0) {
+            let temp = nums[low];
+            nums[low] = nums[mid]
+            nums[mid] = temp;
+            mid++;
+            low++;
+        } else if(nums[mid] == 1) {
+            mid++;
+        } else {
+            let temp = nums[high];
+            nums[high] = nums[mid];
+            nums[mid] = temp;
+            high--;
+        }
     }
-}
-
-return nums;
+};
